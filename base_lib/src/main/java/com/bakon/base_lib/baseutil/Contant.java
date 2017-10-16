7b3713d0edb2deac5ec9b4242f3fddca7ca27f67
bakon <762713299@qq.com>
2017-10-16 23:06:14 +0800

    network

---
+++ base_lib/src/main/java/com/bakon/base_lib/baseutil/Contant.java
@@ -0,0 +1,13 @@
+package com.bakon.base_lib.baseutil;
+
+import com.bakon.base_lib.base.BaseApplication;
+
+import java.io.File;
+
+/**
+ * Created by bakon on 2017/10/16.
+ */
+
+public class Contant {
+    public static final String NET_CACHE = BaseApplication.getInstance().getCacheDir() + File.separator + "NetCache";
+}
---
+++ base_lib/src/main/java/com/bakon/base_lib/net/Network.java
@@ -0,0 +1,102 @@
+package com.bakon.base_lib.net;
+
+import com.bakon.base_lib.baseutil.Contant;
+import com.bakon.base_lib.baseutil.NetUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Cache;
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * 网络操作类
+ */
+
+public class Network {
+
+    private static String baseUrl = "http://gank.io/api/";
+    private static OkHttpClient okHttpClient = null;
+    private static Retrofit retrofit;
+    private static Network network;
+
+    public static Network getInstance() {
+        if (network == null) {
+            network = new Network();
+        }
+        return network;
+    }
+
+    public Network() {
+        initOkHttp();
+        initRetrofit();
+    }
+
+    private static void initOkHttp() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        // 缓存 http://www.jianshu.com/p/93153b34310e
+        File cacheFile = new File(Contant.NET_CACHE);
+        Cache cache = new Cache(cacheFile, 1024 * 1024 * 50);//50M
+        Interceptor cacheInterceptor = new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request();
+                if (!NetUtil.isNetWorkAviliable()) {
+                    request = request.newBuilder()
+                            .cacheControl(CacheControl.FORCE_CACHE)
+                            .build();
+                }
+                Response response = chain.proceed(request);
+                Response.Builder newBuilder = response.newBuilder();
+                if (NetUtil.isNetWorkAviliable()) {
+                    int maxAge = 0;
+                    // 有网络时 设置缓存超时时间0个小时
+                    newBuilder.header("Cache-Control", "public, max-age=" + maxAge);
+                } else {
+                    // 无网络时，设置超时为4周
+                    int maxStale = 60 * 60 * 24 * 28;
+                    newBuilder.header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale);
+                }
+
+                return newBuilder.build();
+            }
+        };
+        builder.cache(cache).addInterceptor(cacheInterceptor);
+
+//        if (BuildConfig.DEBUG) {
+//            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+//            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+//            //设置 Debug Log 模式
+//            builder.addInterceptor(loggingInterceptor);
+//
+//        }
+        //设置超时
+        builder.connectTimeout(15, TimeUnit.SECONDS);
+        builder.readTimeout(20, TimeUnit.SECONDS);
+        builder.writeTimeout(20, TimeUnit.SECONDS);
+        //错误重连
+        builder.retryOnConnectionFailure(true);
+        okHttpClient = builder.build();
+    }
+
+
+    private static void initRetrofit() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .client(okHttpClient)
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build();
+        }
+    }
+
+}
